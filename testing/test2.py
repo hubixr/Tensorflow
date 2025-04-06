6185d5737de6089dc704609128497b8260e0a1ef
hubix_r <hubert.migafka@gmail.com>
2025-04-06 17:29:53 +0200

    Add TensorFlow model implementation for fashion MNIST classification

---
+++ testing/test2.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+# Set random seed
+tf.random.set_seed(42)
+from tensorflow.keras.datasets import fashion_mnist
+
+# The data has already been sorted into training and test sets for us
+(train_data, train_labels), (test_data, test_labels) = fashion_mnist.load_data()
+#normalize the data
+train_data = train_data / 255.0
+test_data = test_data / 255.0
+# Create the model
+model_11 = tf.keras.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)), # input layer (we had to reshape 28x28 to 784, the Flatten layer does this for us)
+  tf.keras.layers.Dense(1024, activation="relu"),
+  tf.keras.layers.Dense(512, activation="relu"),
+  tf.keras.layers.Dense(256, activation="relu"),
+  tf.keras.layers.Dense(128, activation="relu"),
+  tf.keras.layers.Dense(10, activation="softmax") # output shape is 10, activation is softmax
+])
+
+# Compile the model
+model_11.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(), # different loss function for multiclass classifcation
+                 optimizer=tf.keras.optimizers.Adam(),
+                 metrics=["accuracy"])
+
+# Fit the model
+non_norm_history = model_11.fit(train_data,
+                                train_labels,
+                                epochs=5,
+                                batch_size=64,
+                                validation_data=(test_data, test_labels)) # see how the model performs on the test set during training
